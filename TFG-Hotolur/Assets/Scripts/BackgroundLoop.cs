30af0c578741ee21a9fe649ac60a2ee9967d782e
DiegoOya <diegooya5@gmail.com>
2018-05-30 18:22:40 +0200

    BackgroundLoop now works in GameController and it controls all the planes of the background

--- TFG-Hotolur/Assets/Scripts/BackgroundLoop.cs
+++ TFG-Hotolur/Assets/Scripts/BackgroundLoop.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary>
 /// Script used to control the loop of the backgrounds so there can be any number needed
@@ -9,7 +10,7 @@ public class BackgroundLoop : MonoBehaviour {
     private Transform cam;
 
     // SpriteRenderer of the backgrounds
-    private SpriteRenderer[] backgrounds;
+    private List<SpriteRenderer[]> backgrounds = new List<SpriteRenderer[]>();
 
     // The width of the camera
     private float sizeCamX;
@@ -18,20 +19,14 @@ public class BackgroundLoop : MonoBehaviour {
 
     private void Awake()
     {
-        cam = Camera.main.transform;
-        sizeCamX = Camera.main.orthographicSize * 16 / 9;
-        cameraController = cam.GetComponent<CameraController>();
-
-        Plane[] planes = GetComponentsInChildren<Plane>();
-        backgrounds = new SpriteRenderer[planes.Length];
-        for(int i = 0; i < planes.Length; i++)
-        {
-            backgrounds[i] = planes[i].GetComponent<SpriteRenderer>();
-        }
+        AssignComponents();
     }
 
     private void Update()
     {
+        if (cam == null)
+            AssignComponents();
+
         // Position of each side of the camera ([0]: "To the right"; [1]: "To the left") 
         float[] posCameraSides = new float[2];
         posCameraSides[0] = cam.position.x + sizeCamX;
@@ -39,43 +34,71 @@ public class BackgroundLoop : MonoBehaviour {
 
         float camVelX = cameraController.cameraVelX;
 
-        // The camera is moving sideways, so when it points to an area where it is close
-        // to be the end of the background in the camera plane
-        for (int i = 0; i < backgrounds.Length; i++)
+        for (int i = 0; i < backgrounds.Count; i++)
         {
-            if(backgrounds[i].isVisible)
+            SpriteRenderer[] background = backgrounds[i];
+
+            // The camera is moving sideways, so when it points to an area where it is close
+            // to be the end of the background in the camera plane, then move the other background
+            for (int j = 0; j < background.Length; j++)
             {
-                // Position of each side of the background ([0]: "To the right"; [1]: "To the left") 
-                float[] posBGSides = new float[2];
-                posBGSides[0] = backgrounds[i].bounds.center.x + backgrounds[i].bounds.extents.x;
-                posBGSides[1] = backgrounds[i].bounds.center.x - backgrounds[i].bounds.extents.x;
-
-                // If the position of the right side of the camera is near to the 
-                // right end of the background then move the another background
-                if (posCameraSides[0] > posBGSides[0] - 5 && posCameraSides[0] < posBGSides[0] && camVelX > 0)
+                if (background[j].isVisible)
                 {
-                    // Calculate the new position of the background
-                    // ***The argument ([1 - i]) of backgrounds is only valid for 2 backgrounds
-                    float newPosX = backgrounds[i].bounds.center.x + 2 * backgrounds[1 - i].bounds.extents.x;
+                    // Position of each side of the background ([0]: "To the right"; [1]: "To the left") 
+                    float[] posBGSides = new float[2];
+                    posBGSides[0] = background[j].bounds.center.x + background[j].bounds.extents.x;
+                    posBGSides[1] = background[j].bounds.center.x - background[j].bounds.extents.x;
 
-                    backgrounds[1 - i].transform.position = new Vector3(newPosX, 
-                                                                        backgrounds[1 - i].transform.position.y, 
-                                                                        backgrounds[1 - i].transform.position.z);
-                }
+                    // If the position of the right side of the camera is near to the 
+                    // right end of the background then move the another background
+                    if (posCameraSides[0] > posBGSides[0] - 5 && posCameraSides[0] < posBGSides[0] && camVelX > 0)
+                    {
+                        // Calculate the new position of the background
+                        // ***The argument ([1 - j]) of background is only valid for 2 background
+                        float newPosX = background[j].bounds.center.x + 2 * background[1 - j].bounds.extents.x;
 
-                // If the position of the left side of the camera is near to the 
-                // left end of the background then move the another background
-                if (posCameraSides[1] > posBGSides[1] && posCameraSides[1] < posBGSides[1] + 5 && camVelX < 0)
-                {
-                    // Calculate the new position of the background
-                    // ***The argument ([1 - i]) of backgrounds is only valid for 2 backgrounds
-                    float newPosX = backgrounds[i].bounds.center.x - 2 * backgrounds[1 - i].bounds.extents.x;
+                        background[1 - j].transform.position = new Vector3(newPosX,
+                                                                            background[1 - j].transform.position.y,
+                                                                            background[1 - j].transform.position.z);
+                    }
 
-                    backgrounds[1 - i].transform.position = new Vector3(newPosX,
-                                                                        backgrounds[1 - i].transform.position.y,
-                                                                        backgrounds[1 - i].transform.position.z);
+                    // If the position of the left side of the camera is near to the 
+                    // left end of the background then move the another background
+                    if (posCameraSides[1] > posBGSides[1] && posCameraSides[1] < posBGSides[1] + 5 && camVelX < 0)
+                    {
+                        // Calculate the new position of the background
+                        // ***The argument ([1 - j]) of background is only valid for 2 background
+                        float newPosX = background[j].bounds.center.x - 2 * background[1 - j].bounds.extents.x;
+
+                        background[1 - j].transform.position = new Vector3(newPosX,
+                                                                            background[1 - j].transform.position.y,
+                                                                            background[1 - j].transform.position.z);
+                    }
                 }
             }
+        }
+    }
+
+    // Assign all the references 
+    void AssignComponents()
+    {
+        cam = Camera.main.transform;
+        sizeCamX = Camera.main.orthographicSize * 16 / 9;
+        cameraController = cam.GetComponent<CameraController>();
+
+        // Search for all the backgrounds in the scene and get the SpriteRenderers of each background
+        GameObject[] backgroundsGO = GameObject.FindGameObjectsWithTag(Tags.background);
+
+        backgrounds.Clear();
+        for (int i = 0; i < backgroundsGO.Length; i++)
+        {
+            Plane[] planes = backgroundsGO[i].GetComponentsInChildren<Plane>();
+            SpriteRenderer[] spriteRenderersBG = new SpriteRenderer[planes.Length];
+            for (int j = 0; j < planes.Length; j++)
+            {
+                spriteRenderersBG[j] = planes[j].GetComponent<SpriteRenderer>();
+            }
+            backgrounds.Add(spriteRenderersBG);
         }
     }
 
